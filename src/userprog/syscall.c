aee6fd62d9f5ac748cfd3e58bf10650dc9212e38
Wu Jiaxing <wujx2023@shanghaitech.edu.cn>
2025-04-15 23:23:17 +0800

    feat: user mem check

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,9 +5,37 @@
 #include <syscall-nr.h>
 
 static void syscall_handler(struct intr_frame *);
+static int get_user (const uint8_t *uaddr);
 
 void syscall_init(void) {
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+void check_address(const void *addr) {
+  // Check if the address is within the user address space
+  if(!is_user_vaddr(addr)) {
+  // TODO: exit codes
+  }
+  // Check if the address is not NULL
+  void *ptr = pagedir_get_page(thread_current()->pagedir, addr);
+  if(!ptr){
+    //TOdo: exit codes
+  }
+  uint8_t *check_by = (uint8_t *) addr;
+  for (uint8_t i = 0; i < 4; i++){
+    if(get_user(check_by + i) == -1){
+      //TODO: exit codes
+    }
+  }
+  return ptr;
+}
+
+static int 
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:" : "=&a" (result) : "m" (*uaddr));
+  return result;
 }
 
 static void syscall_handler(struct intr_frame *f UNUSED) {
