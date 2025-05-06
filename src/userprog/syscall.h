c5f4bcdb9f1af5ee85005022de112575cad96d1f
Renyi Yang <yangry2023@shanghaitech.edu.cn>
2025-05-07 02:00:18 +0800

    feat: mmem syscall structure

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+typedef int mapid_t;
+
 void syscall_init(void);
+
+void exit(int);
+void munmap(mapid_t);
 
 #endif /* userprog/syscall.h */
