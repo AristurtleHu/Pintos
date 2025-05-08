5757e6aa6f53f247f2a0bfd32dcd77ce289d07d1
Renyi Yang <yangry2023@shanghaitech.edu.cn>
2025-05-09 01:55:02 +0800

    fix: typo

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -145,8 +145,7 @@ static void page_fault(struct intr_frame *f) {
 
   void *esp = f->esp;
 
-  /* If a page fault occurs in kernel mode,
-     use the esp saved in syscall. */
+  /* Kernel mode, save the esp */
 #ifdef VM
   if (!user)
     esp = thread_current()->esp;
@@ -157,7 +156,7 @@ static void page_fault(struct intr_frame *f) {
   /* Error */
   if (!fault_addr || !not_present || (is_kernel_vaddr(fault_addr) && user)) {
 
-    // user error
+    // kernel error
     if (!user) {
       f->eip = (void *)f->eax;
       f->eax = -1;
