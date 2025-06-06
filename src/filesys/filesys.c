bbf3aba14448654a3670d0f4552e7c23737d341b
Renyi Yang <yangry2023@shanghaitech.edu.cn>
2025-06-07 04:22:56 +0800

    feat: subdir with filesystem

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -44,10 +44,11 @@ void filesys_done(void) {
    or if internal memory allocation fails. */
 bool filesys_create(const char *name, off_t initial_size, bool is_dir) {
   block_sector_t inode_sector = 0;
-  struct dir *dir = dir_open_path(name);
+  const char *file_name = NULL;
+  struct dir *dir = dir_split(name, &file_name);
   bool success = (dir != NULL && free_map_allocate(1, &inode_sector) &&
                   inode_create(inode_sector, initial_size, is_dir) &&
-                  dir_add(dir, name, inode_sector, is_dir));
+                  dir_add(dir, file_name, inode_sector, is_dir));
   if (!success && inode_sector != 0)
     free_map_release(inode_sector, 1);
   dir_close(dir);
@@ -61,12 +62,16 @@ bool filesys_create(const char *name, off_t initial_size, bool is_dir) {
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
 struct file *filesys_open(const char *name) {
-  struct dir *dir = dir_open_path(name);
-  struct inode *inode = NULL;
+  const char *file_name = NULL;
+  struct dir *dir = dir_split(name, &file_name);
+  if (dir == NULL)
+    return NULL;
 
-  if (dir != NULL)
-    dir_lookup(dir, name, &inode);
-  dir_close(dir);
+  struct inode *inode = dir_get_inode(dir);
+  if (strlen(file_name) > 0) {
+    dir_lookup(dir, file_name, &inode);
+    dir_close(dir);
+  }
 
   return file_open(inode);
 }
@@ -76,8 +81,9 @@ struct file *filesys_open(const char *name) {
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
 bool filesys_remove(const char *name) {
-  struct dir *dir = dir_open_path(name);
-  bool success = dir != NULL && dir_remove(dir, name);
+  const char *file_name = NULL;
+  struct dir *dir = dir_split(name, &file_name);
+  bool success = dir != NULL && dir_remove(dir, file_name);
   dir_close(dir);
 
   return success;
@@ -94,9 +100,20 @@ static void do_format(void) {
 }
 
 bool filesys_chdir(const char *name) {
-  struct dir *dir = dir_open_path(name);
-  if (dir == NULL)
+  const char *file_name = NULL;
+  /* Open the directory except the last token */
+  struct dir *dir = dir_split(name, &file_name);
+  if (!dir)
     return false;
+
+  if (strlen(name) > 0) {
+    struct inode *inode = NULL;
+    dir_lookup(dir, file_name, &inode);
+    dir_close(dir);
+    dir = dir_open(inode);
+    if (!dir)
+      return false;
+  }
 
   dir_close(thread_current()->cwd); // Close old
   thread_current()->cwd = dir;      // Set new
