7dac9d91519606b76a56901fd6c7e53d925c6f7c
Renyi Yang <yangry2023@shanghaitech.edu.cn>
2025-06-07 22:39:19 +0800

    fix: inode no public access

--- src/filesys/inode.h
+++ src/filesys/inode.h
@@ -6,27 +6,6 @@
 #include <list.h>
 #include <stdbool.h>
 
-/* On-disk inode.
-   Must be exactly BLOCK_SECTOR_SIZE bytes long. */
-struct inode_disk {
-  block_sector_t start; /* First data sector. */
-  off_t length;         /* File size in bytes. */
-  unsigned magic;       /* Magic number. */
-  uint32_t unused[125]; /* Not used. */
-  bool is_dir;          /* True if this inode is a directory. */
-};
-
-/* In-memory inode. */
-struct inode {
-  struct list_elem elem;  /* Element in inode list. */
-  block_sector_t sector;  /* Sector number of disk location. */
-  int open_cnt;           /* Number of openers. */
-  bool removed;           /* True if deleted, false otherwise. */
-  int deny_write_cnt;     /* 0: writes ok, >0: deny writes. */
-  struct inode_disk data; /* Inode content. */
-  bool is_dir;            /* True if this inode is a dir. */
-};
-
 struct bitmap;
 
 void inode_init(void);
@@ -43,5 +22,6 @@ void inode_allow_write(struct inode *);
 off_t inode_length(const struct inode *);
 
 bool inode_is_directory(const struct inode *inode);
+bool inode_is_removed(const struct inode *inode);
 
 #endif /* filesys/inode.h */
